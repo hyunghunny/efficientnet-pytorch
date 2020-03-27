69979463212310bc7e936c401bca3ac0c5541698
Hyunghun Cho <hyunghunny@gmail.com>
2020-03-27 20:31:15 +0900

    refactored

--- train.py
+++ train.py
@@ -34,41 +34,50 @@ def init_process(backend, init_method, world_size, rank):
 
 
 def main():    
-    torch.backends.cudnn.benchmark = True
-
     args = parse_args()
-    config = mlconfig.load(args.config)
-    print(config)
+    train(args.config, args.backend, args.init_method, 
+          args.world_size, args.rank, args.no_cuda,
+          args.resume, args.data_parallel)
 
-    if args.world_size > 1:
-        init_process(args.backend, args.init_method, args.world_size, args.rank)
 
-    device = torch.device('cuda' if torch.cuda.is_available() and not args.no_cuda else 'cpu')
+def train(config, 
+          backend='nccl', init_method='tcp://127.0.0.1:23456', world_size=1, rank=0, 
+          no_cuda=False, resume=None, data_parallel=False
+          ):
+    t_start = time.time()
+    torch.backends.cudnn.benchmark = True
+    cfg = mlconfig.load(config)
+    print(cfg)
 
-    model = config.model()
+    if world_size > 1:
+        init_process(backend, init_method, world_size, rank)
+
+    device = torch.device('cuda' if torch.cuda.is_available() and not no_cuda else 'cpu')
+
+    model = cfg.model()
     if distributed_is_initialized():
         model.to(device)
         model = nn.parallel.DistributedDataParallel(model)
     else:
-        if args.data_parallel:
+        if data_parallel:
             model = nn.DataParallel(model)
         model.to(device)
 
-    optimizer = config.optimizer(model.parameters())
-    scheduler = config.scheduler(optimizer)
-    train_loader = config.dataset(train=True)
-    valid_loader = config.dataset(train=False)
+    optimizer = cfg.optimizer(model.parameters())
+    scheduler = cfg.scheduler(optimizer)
+    train_loader = cfg.dataset(train=True)
+    valid_loader = cfg.dataset(train=False)
 
-    trainer = config.trainer(model, optimizer, train_loader, valid_loader, scheduler, device)
+    trainer = cfg.trainer(model, optimizer, train_loader, valid_loader, scheduler, device)
 
-    if args.resume is not None:
-        trainer.resume(args.resume)
+    if resume is not None:
+        trainer.resume(resume)
 
     trainer.fit()
+    t_end = time.time() - t_start
+    print("Total training time: {:.1f}".format(t_end))
 
 
 if __name__ == "__main__":
-    t_start = time.time()
     main()
-    t_end = time.time() - t_start
-    print("Total training time: {:.1f}".format(t_end))
+    
