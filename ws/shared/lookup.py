956acfd667e733244c8e588469b392d4a9efe070
Hyunghun Cho <hyunghunny@gmail.com>
2020-03-27 23:36:42 +0900

    pandas fix

--- ws/shared/lookup.py
+++ ws/shared/lookup.py
@@ -1,6 +1,5 @@
 import os
 
-import pandas as pd
 import numpy as np
 
 from ws.shared.read_cfg import read_hyperparam_config
@@ -19,6 +18,8 @@ def check_lookup_existed(name, lookup_dir=LOOKUP_DIR):
 
 
 def load(data_type, lookup_dir=LOOKUP_DIR, config_folder='hp_conf/', grid_order=None):
+    import pandas as pd
+
     grid_shuffle = False
     if grid_order == 'shuffle':
         grid_shuffle = True
